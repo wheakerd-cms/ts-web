73b4e68413176336f4b98079c5f84e03de246168
wheakerd <wheakerd@gmail.com>
2024-09-24 19:16:24 +0800

    feat(*): init project

--- src/app/admin/router/index.ts
+++ src/app/admin/router/index.ts
@@ -1,7 +1,5 @@
 import {createRouter, createWebHistory} from 'vue-router';
 
-console.log(window.location)
-
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes: [
