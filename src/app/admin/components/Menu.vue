1ff16e9cb3763823acd105e48948fb07a34ff46e
wheakerd <wheakerd@gmail.com>
2024-10-07 22:03:42 +0800

    temp(*)

--- src/app/admin/components/Menu.vue
+++ src/app/admin/components/Menu.vue
@@ -1,171 +1,100 @@
 <script lang="tsx">
 import {defineComponent} from "vue";
 import {
-    ElMenu,
-    ElSubMenu,
-    ElMenuItemGroup,
-    ElMenuItem,
-    ElIcon,
+	ElMenu,
+	ElSubMenu,
+	ElMenuItemGroup,
+	ElMenuItem,
+	ElIcon,
 } from "element-plus";
 import {
-    Menu,
-    Document,
-    Location,
-    Setting,
+	Menu,
+	Document,
+	Location,
+	Setting,
 } from "@element-plus/icons-vue";
 
 export default defineComponent({
-    name: "MenuComponent",
-    computed: {},
-    props: {
-        className: {},
-    },
-    setup(props, {slots}) {
-        const handleOpen = () => {
-        };
+	name: "MenuComponent",
+	computed: {},
+	props: {
+		className: {},
+		backgroundColor: '',
+	},
+	setup(props) {
+		console.log(props)
+		const handleOpen = () => {
+		};
 
-        const handleClose = () => {
-        };
+		const handleClose = () => {
+		};
 
-        return () => (
-            <>
-                <ElMenu active-text-color="#ffd04b"
-                        background-color="#545c64"
-                        class={`el-menu-vertical-demo ${props.className}`}
-                        default-active="2"
-                        text-color="#fff"
-                        onOpen={handleOpen}
-                        onClose={handleClose}
-                >
-                    <ElSubMenu index="1" v-slots={{
-                        title: () => {
-                            return (<>
-                                <ElIcon>
-                                    <Location/>
-                                </ElIcon>
-                                <span>Navigator One</span>
-                            </>);
-                        },
-                    }}>
-                        <ElMenuItemGroup title="Group One">
-                            <ElMenuItem index="1-1">item one</ElMenuItem>
-                            <ElMenuItem index="1-2">item two</ElMenuItem>
-                        </ElMenuItemGroup>
-                        <ElMenuItemGroup title="Group Two">
-                            <ElMenuItem index="1-3">item three</ElMenuItem>
-                        </ElMenuItemGroup>
-                        <ElSubMenu index="1-4" v-slots={{
-                            title: () => 'item four',
-                        }}>
-                            <ElMenuItem index="1-4-1">item one</ElMenuItem>
-                        </ElSubMenu>
-                    </ElSubMenu>
-                    <ElMenuItem index="2">
-                        <ElIcon>
-                            <Menu/>
-                        </ElIcon>
-                        <span>Navigator Two</span>
-                    </ElMenuItem>
-                    <ElMenuItem index="3" disabled>
-                        <ElIcon>
-                            <Document/>
-                        </ElIcon>
-                        <span>Navigator Three</span>
-                    </ElMenuItem>
-                    <ElMenuItem index="4">
-                        <ElIcon>
-                            <Setting/>
-                        </ElIcon>
-                        <span>Navigator Four</span>
-                    </ElMenuItem><ElMenuItem index="2">
-                        <ElIcon>
-                            <Menu/>
-                        </ElIcon>
-                        <span>Navigator Two</span>
-                    </ElMenuItem>
-                    <ElMenuItem index="3" disabled>
-                        <ElIcon>
-                            <Document/>
-                        </ElIcon>
-                        <span>Navigator Three</span>
-                    </ElMenuItem>
-                    <ElMenuItem index="4">
-                        <ElIcon>
-                            <Setting/>
-                        </ElIcon>
-                        <span>Navigator Four</span>
-                    </ElMenuItem><ElMenuItem index="2">
-                        <ElIcon>
-                            <Menu/>
-                        </ElIcon>
-                        <span>Navigator Two</span>
-                    </ElMenuItem>
-                    <ElMenuItem index="3" disabled>
-                        <ElIcon>
-                            <Document/>
-                        </ElIcon>
-                        <span>Navigator Three</span>
-                    </ElMenuItem>
-                    <ElMenuItem index="4">
-                        <ElIcon>
-                            <Setting/>
-                        </ElIcon>
-                        <span>Navigator Four</span>
-                    </ElMenuItem><ElMenuItem index="2">
-                        <ElIcon>
-                            <Menu/>
-                        </ElIcon>
-                        <span>Navigator Two</span>
-                    </ElMenuItem>
-                    <ElMenuItem index="3" disabled>
-                        <ElIcon>
-                            <Document/>
-                        </ElIcon>
-                        <span>Navigator Three</span>
-                    </ElMenuItem>
-                    <ElMenuItem index="4">
-                        <ElIcon>
-                            <Setting/>
-                        </ElIcon>
-                        <span>Navigator Four</span>
-                    </ElMenuItem><ElMenuItem index="2">
-                        <ElIcon>
-                            <Menu/>
-                        </ElIcon>
-                        <span>Navigator Two</span>
-                    </ElMenuItem>
-                    <ElMenuItem index="3" disabled>
-                        <ElIcon>
-                            <Document/>
-                        </ElIcon>
-                        <span>Navigator Three</span>
-                    </ElMenuItem>
-                    <ElMenuItem index="4">
-                        <ElIcon>
-                            <Setting/>
-                        </ElIcon>
-                        <span>Navigator Four</span>
-                    </ElMenuItem><ElMenuItem index="2">
-                        <ElIcon>
-                            <Menu/>
-                        </ElIcon>
-                        <span>Navigator Two</span>
-                    </ElMenuItem>
-                    <ElMenuItem index="3" disabled>
-                        <ElIcon>
-                            <Document/>
-                        </ElIcon>
-                        <span>Navigator Three</span>
-                    </ElMenuItem>
-                    <ElMenuItem index="4">
-                        <ElIcon>
-                            <Setting/>
-                        </ElIcon>
-                        <span>Navigator Four</span>
-                    </ElMenuItem>
-                </ElMenu>
-            </>
-        );
-    },
+		return () => (
+			<>
+				<ElMenu active-text-color="#ffd04b"
+						background-color="#545c64"
+						class={`el-menu-vertical-demo ${props.className}`}
+						style={`${props.style}`}
+						default-active="2"
+						text-color="#fff"
+						onOpen={handleOpen}
+						onClose={handleClose}
+				>
+					<ElSubMenu index="1" v-slots={{
+						title: () => {
+							return (<>
+								<ElIcon>
+									<Location/>
+								</ElIcon>
+								<span>Navigator One</span>
+							</>);
+						},
+					}}>
+						<ElMenuItemGroup title="Group One">
+							<ElMenuItem index="1-1">item one</ElMenuItem>
+							<ElMenuItem index="1-2">item two</ElMenuItem>
+						</ElMenuItemGroup>
+						<ElMenuItemGroup title="Group Two">
+							<ElMenuItem index="1-3">item three</ElMenuItem>
+						</ElMenuItemGroup>
+						<ElSubMenu index="1-4" v-slots={{
+							title: () => 'item four',
+						}}>
+							<ElMenuItem index="1-4-1">item one</ElMenuItem>
+						</ElSubMenu>
+					</ElSubMenu>
+					<ElMenuItem index="2">
+						<ElIcon>
+							<Menu/>
+						</ElIcon>
+						<span>Navigator Two</span>
+					</ElMenuItem>
+					<ElMenuItem index="3" disabled>
+						<ElIcon>
+							<Document/>
+						</ElIcon>
+						<span>Navigator Three</span>
+					</ElMenuItem>
+					<ElMenuItem index="4">
+						<ElIcon>
+							<Setting/>
+						</ElIcon>
+						<span>Navigator Four</span>
+					</ElMenuItem>
+					<ElMenuItem index="2">
+						<ElIcon>
+							<Menu/>
+						</ElIcon>
+						<span>Navigator Two</span>
+					</ElMenuItem>
+				</ElMenu>
+			</>
+		);
+	},
 });
 </script>
+<style lang="css">
+.el-menu-vertical-demo {
+	--el-menu-bg-color: #001529 !important;
+}
+</style>
--- src/app/admin/views/LayoutView.vue
+++ src/app/admin/views/LayoutView.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
+import {type Ref, ref} from "vue";
 import MenuComponent from "@/app/admin/components/Menu.vue";
 import logoImage from "@/assets/logo.svg";
+
+const datasource: Ref = ref([]);
 
 </script>
 <template>
@@ -8,13 +11,19 @@ import logoImage from "@/assets/logo.svg";
 		<nav :style="{
 			 width: '220px',
 			 height: '100vh',
+			 backgroundColor: '#001529',
 		}">
-			<div class="d-flex align-items-center py-2">
-				<img :src="logoImage"
-					 alt="logo" style="width: 32px;"/>
-				<span class="ms-2">后台管理系统</span>
+			<div class="d-flex align-items-center justify-content-center py-2">
+				<div>
+					<img :src="logoImage"
+						 alt="logo" style="width: 32px;"/>
+					<span class="ms-2">后台管理系统</span>
+				</div>
 			</div>
-			<MenuComponent class-name="h-100 overflow-y-auto"/>
+			<MenuComponent class-name="h-100 overflow-y-auto border-0"
+						   backgroundColor="#001529"
+						   :dataSource="datasource"
+			/>
 		</nav>
 	</div>
 </template>
