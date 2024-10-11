136a304d90e11226f69bdedf457795545fd5e43e
wheakerd <wheakerd@gmail.com>
2024-10-11 18:54:00 +0800

    feat(*): This commit only records automatic derivation, reverse derivation from the component library

--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -1,55 +1,66 @@
-import {
-	ElCascader,
-	ElCheckboxGroup,
-	ElColorPicker,
-	ElDatePicker,
-	ElInput,
-	ElInputNumber,
-	ElRadioGroup,
-	ElRate,
-	ElSelect,
-	ElSelectV2,
-	ElSlider,
-	ElSwitch,
-	ElTimePicker,
-	ElTimeSelect,
-	ElTransfer,
-	ElAutocomplete,
-	ElDivider,
-	ElTreeSelect,
-	ElUpload,
-} from "element-plus";
+import type {SFCWithInstall} from 'element-plus/es/utils';
+import {GlobalComponents} from 'vue';
+import type {ComponentPublicInstance, ComputedOptions, DefineComponent, MethodOptions} from "@vue/runtime-core";
 
-export const formComponents = {
-	ElCascader,
-	ElCheckboxGroup,
-	ElColorPicker,
-	ElDatePicker,
-	ElInput,
-	ElInputNumber,
-	ElRadioGroup,
-	ElRate,
-	ElSelect,
-	ElSelectV2,
-	ElSlider,
-	ElSwitch,
-	ElTimePicker,
-	ElTimeSelect,
-	ElTransfer,
-	ElAutocomplete,
-	ElDivider,
-	ElTreeSelect,
-	ElUpload,
-} as const;
+type ComponentsType = { [key: string]: unknown };
 
-export type FormComponentsKeys = keyof typeof formComponents;
+interface Components extends GlobalComponents, ComponentsType {
+}
 
-type FormComponents = typeof formComponents [FormComponentsKeys];
-
-export type FormComponentsSlots<T> = T extends { $slots: infer S }
-	? keyof S extends infer K
-		? K extends string
-			? K
+export type ComponentsSlots<T extends Components = keyof T> =
+	Components [T] extends SFCWithInstall<infer C>
+		? C extends C & {
+			new(): {
+				$slots: infer S;
+			};
+		}
+			? {
+				[K in keyof S as string extends K
+					? never
+					: number extends K
+						? never
+						: symbol extends K
+							? never
+							: K]?: (_: any) => JSX.Element;
+			}
 			: never
-		: never
-	: never;
+		: never;
+
+type ComponentPublicInstanceConstructor<T extends ComponentPublicInstance<Props, RawBindings, D, C, M> = ComponentPublicInstance<any>, Props = any, RawBindings = any, D = any, C extends ComputedOptions = ComputedOptions, M extends MethodOptions = MethodOptions> = {
+	__isFragment?: never;
+	__isTeleport?: never;
+	__isSuspense?: never;
+	new(...args: any[]): T;
+};
+
+type ComponentsAttributes<T extends Components = keyof T> =
+	Components [T] extends SFCWithInstall<infer C>
+		? C extends infer C & {
+			new(): {
+				$slots: infer S;
+			};
+		}
+			? C extends ComponentPublicInstanceConstructor<
+					infer AA,
+					infer AB
+				>
+				? AA ['$props']
+				: never
+			: never
+		: never;
+
+
+export interface ComponentField<T extends Components = keyof T> {
+	field: string;
+	label: string;
+	show?: boolean;
+	component: T;
+	attributes?: ComponentsAttributes<T>;
+	slots: ComponentsSlots<T>;
+}
+
+interface ComponentElInput extends Omit<ComponentField<'ElInput'>, 'component'> {
+	events: {
+		blur: (event: Function) => void;
+	}
+}
